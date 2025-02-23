55d50a33436ae6416fd6c614ff0b7c6252d80643
JetLearnAdhayan <adhayanraj9@gmail.com>
2025-02-23 12:54:24 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#Steps to be followed in a machine learning project 
+#a) gathering the data and load it 
+#b) preprocess the data - for future machine learning projects
+#c) Analyse the data yourself and define your input and output coloumns and remove the rest
+#d) define input and output - create separate dataframes for input and output
+#e) split 80% of the data for training purposes and 20% for testing
+#f) select the machine learning algorithim you're going to use
+#g) compare the pridictions with the acutal results to fid out accuracy
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+
+print(dataset.head())
+print(dataset.info())
+
+#identifying input(features) and output(target)
+#output = purchased or not
+#input = country, age, salary
+
+X = dataset.iloc[:, :-1].values  #picked all but last column
+y = dataset.iloc[:, -1].values #picked only last coloumn
+
+print("Features :\n",X)
+print("Target :\n",y)
+print()
